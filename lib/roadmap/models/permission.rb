8d703d233b87cd1b47e85110a6a388907932361e
Jack Polgar <nrx@nirix.net>
2013-02-09 07:17:25 +1100

    Permission and User models updated.

--- lib/roadmap/models/permission.rb
+++ lib/roadmap/models/permission.rb
@@ -1,6 +1,6 @@
 #
 # Roadmap
-# Copyright (C) 2012 Jack Polgar
+# Copyright (C) 2012-2013 J. Polgar
 # https://github.com/nirix
 #
 # Roadmap is free software: you can redistribute it and/or modify
@@ -16,26 +16,42 @@
 # along with Roadmap. If not, see <http://www.gnu.org/licenses/>.
 #
 
-class Permission < Sequel::Model
-  def can?(context, project_id, action)
-    action = action.to_s
+module Roadmap
+  module Models
+    class Permission < Sequel::Model
+      ##
+      # Checks if the action can be performed on the project
+      # by the specified context, such as a Group.
+      #
+      # @param [Object]  context    Context, such as Group, Role, etc.
+      # @param [Integer] project_id Project ID
+      # @param [Symbol]  action     Action being performed
+      #
+      # @return [Boolean]
+      #
+      def self.can?(context, project_id, action)
+        action = action.to_s
 
-    default = find(context: context.class, context_id: 0, project_id: 0, action: action)
-    project_default = find(ccontext: context.class, ontext_id: context.id, project_id: project_id, action: action)
-    actual = find(context: context.class, context_id: context.id, project_id: project.id, action: action)
+        context_class = context.class.to_s.split('::').last
 
-    if actual
-      actual.allowed?
-    elsif project_default
-      project_default.allowed?
-    elsif default
-      default.allowed?
-    else
-      false
+        default = find(context: context_class, context_id: 0, project_id: 0, action: action)
+        project_default = find(context: context_class, context_id: context.id, project_id: project_id, action: action)
+        actual = find(context: context_class, context_id: context.id, project_id: project_id, action: action)
+
+        if actual
+          actual.allowed?
+        elsif project_default
+          project_default.allowed?
+        elsif default
+          default.allowed?
+        else
+          false
+        end
+      end
+
+      def allowed?
+        value == 1 or value == true
+      end
     end
-  end
-
-  def allowed?
-    value == 1 or value == true
   end
 end
--- lib/roadmap/models/user.rb
+++ lib/roadmap/models/user.rb
@@ -1,6 +1,6 @@
 #
 # Roadmap
-# Copyright (C) 2012 Jack Polgar
+# Copyright (C) 2012-2013 J. Polgar
 # https://github.com/nirix
 #
 # Roadmap is free software: you can redistribute it and/or modify
@@ -16,23 +16,57 @@
 # along with Roadmap. If not, see <http://www.gnu.org/licenses/>.
 #
 
-require 'bcrypt'
+module Roadmap
+  module Models
+    class User < Sequel::Model
+      many_to_one :group
 
-class User < Sequel::Model
-  def href
-    "/users/#{id}"
-  end
+      ##
+      # Returns the path for the users profile.
+      #
+      # @return [String]
+      #
+      def href
+        "/profiles/#{id}"
+      end
 
-  def can?(context, action, project_id = nil)
-    project_id = @current_project.id if project_id == nil
-    Permission.can?(context, project_id, action)
-  end
+      ##
+      # Checks if the user has permission to perform
+      # the specified action.
+      #
+      # @param [Symbol]  action     Action being performed
+      # @param [Integer] project_id Project ID
+      #
+      # @return [Boolean]
+      #
+      def can?(action, project_id = nil)
+        project_id = @current_project.id if project_id.nil?
+        puts group.name
+        Permission.can?(group, project_id, action)
+      end
 
-  def self.check_credentials(user, pass)
-    if u = find(username: user) and BCrypt::Password.new(pass) == u.password
-      u
-    else
-      false
+      ##
+      # Checks if the password matches the users password.
+      #
+      # @return [Boolean]
+      #
+      def check_password(pass)
+        BCrypt::Password.new(pass) == password
+      end
+
+      ##
+      # Checks the username and password and returns the
+      # user if one if found.
+      #
+      # @return [Object]
+      #
+      def self.check_credentials(user, pass)
+        if user = find(username: user) and user.check_password(pass)
+          u
+        end
+
+        false
+      end
     end
   end
 end
