b92dbc6b521ff5a640231a82dc3868a32963a324
Jack Polgar <nrx@nirix.net>
2013-02-09 03:49:18 +1100

    Updated Project and Setting models.

--- lib/roadmap/models/project.rb
+++ lib/roadmap/models/project.rb
@@ -1,35 +1,17 @@
-#
-# Roadmap
-# Copyright (C) 2012 Nirix
-# https://github.com/nirix
-#
-# Roadmap is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 3 only.
-#
-# Roadmap is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Roadmap. If not, see <http://www.gnu.org/licenses/>.
-#
 
-class Project < Sequel::Model
-  plugin :sluggable, :source => :name, :frozen => false
+module Roadmap
+  module Models
+    class Project < Sequel::Model
+      plugin :sluggable, source: :name, frozen: false
 
-  one_to_many :issues
-
-  def self.is_project(slug)
-    if p = find(:slug => slug)
-      p
-    else
-      false
+      ##
+      # Returns the URI for the project
+      #
+      # @return [String]
+      #
+      def href(uri = nil)
+        slug + (!uri.nil? ? "/#{uri}" :'')
+      end
     end
-  end
-
-  def href(uri = '')
-    "/#{slug}#{uri}"
   end
 end
