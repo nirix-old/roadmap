cefd6483fd7869aef7701e7a7a90dbc61f4a9b99
Jack Polgar <nrx@nirix.net>
2013-02-27 12:26:48 +1100

    Added the `devserv.rb` file to easily start Roadmap without having to first build and install.

---
+++ devserv.rb
@@ -0,0 +1,15 @@
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'roadmap'
+
+Roadmap.config do |app|
+  app.set public_folder: File.dirname(__FILE__) + "/proto/public"
+  app.set port: ENV['PORT'] || 3000
+  app.set session_secret: "change-this"
+  #app.set environment: :production
+
+  app.database = Sequel.connect('mysql2://root:root@localhost/roadmap')
+end
+
+Roadmap.start
