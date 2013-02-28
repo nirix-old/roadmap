75162c5c1e54136b065a248243f9c328c1fe7b6c
Jack Polgar <nrx@nirix.net>
2013-02-28 14:58:00 +1100

    Set development environment in `devserv.rb`.

--- devserv.rb
+++ devserv.rb
@@ -7,7 +7,7 @@ Roadmap.config do |app|
   app.set public_folder: File.dirname(__FILE__) + "/proto/public"
   app.set port: ENV['PORT'] || 3000
   app.set session_secret: "change-this"
-  #app.set environment: :production
+  app.set environment: :development
 
   app.database = Sequel.connect('mysql2://root:root@localhost/roadmap')
 end
